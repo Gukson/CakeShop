5450677eefc3dc7dde3b8f7ee65c4221df0ed5bb
Gukson <75785907+Gukson@users.noreply.github.com>
2023-07-16 20:17:19 +0200

    Co-authored-by: rwlodarczyk <rwlodarczyk@users.noreply.github.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,11 +2,9 @@ import Works from "../components/layout/works";
 import About from "../components/layout/about";
 import Logo from "../components/layout/logo";
 import Footer from "../components/navigation/footer";
-import Cakes from "../components/layout/cakes";
 const Home = () => {
   return (
     <div>
-      <Cakes/>
       <Logo />
       <Works />
       <About />
