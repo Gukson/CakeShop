a0b4c91f2f0f272ca946f24d95aa9e4501784962
Gukson <75785907+Gukson@users.noreply.github.com>
2023-07-21 01:20:31 +0200

    some changes with navbar

--- src/components/navigation/menu.tsx
+++ src/components/navigation/menu.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {IconButton, Menu, MenuItem} from "@mui/material";
 type IconedMenuProps = {
     icon: React.ReactNode
 }
 
 export const IconedMenu = ({icon}: IconedMenuProps) =>
 {
-    return (<>{icon}</>)
+    const [open, setOpen] = useState(false)
+
+    return (<>
+        <Menu open={open} onClose={()=>setOpen(false)}>
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>My account</MenuItem>
+            <MenuItem>Logout</MenuItem>
+        </Menu>
+        <IconButton onClick={() => setOpen(!open)}>{icon}</IconButton>
+    </>)
 }
